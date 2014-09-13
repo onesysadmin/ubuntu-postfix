9ec078582a6d4f4d8e45c57f8231b6edd2b34971
Alex Sanz <asans@evirtualpost.com>
2014-09-12 19:12:13 -0700

    do not remove files in my own directory

--- deploy/provision.sh
+++ deploy/provision.sh
@@ -18,4 +18,3 @@ apt-get -qq clean
 rm -f /etc/dpkg/dpkg.cfg.d/02apt-speedup
 find /var/lib/apt/lists -mindepth 1 -delete -print
 find /tmp /var/tmp -mindepth 2 -delete -print
-find . -delete -print
